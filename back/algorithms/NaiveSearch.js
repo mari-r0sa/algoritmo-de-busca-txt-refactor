e4a7724d37146de281e1372fd7c5583fab7b7a8b
Mari Rosa Oliveira <mari.rosa@totvs.com.br>
2026-04-28 11:33:22 -0300

    pequenos ajustes

--- back/algorithms/NaiveSearch.js
+++ back/algorithms/NaiveSearch.js
@@ -25,14 +25,8 @@ export default class NaiveSearch extends SearchStrategy {
   }
 
   *searchStepByStep(text, pattern) {
-    let comparisons = 0;
-    const matches = [];
-
     const pre = this._ensureInputs(text, pattern);
-    if (pre.skip) {
-      return pre.result;
-    }
-    const start = pre.start;
+    if (pre.skip) return;
 
     for (let i = 0; i <= text.length - pattern.length; i++) {
       let j = 0;
@@ -43,19 +37,11 @@ export default class NaiveSearch extends SearchStrategy {
           j,
           textChar: text[i + j],
           patternChar: pattern[j],
-          comparisons: comparisons + 1,
-          matches: [...matches]
         };
-
-        comparisons++;
 
         if (text[i + j] !== pattern[j]) break;
         j++;
       }
-
-      if (j === pattern.length) matches.push(i);
     }
-
-    return this._createResult(matches, comparisons, start);
   }
 }
